c8887b81fbb5f0c0480023c89c7dc38729a11a75
johnneon <neon.1598@mail.ru>
2021-03-22 02:13:34 +0600

    fix: fix port

--- src/bin/www.js
+++ src/bin/www.js
@@ -2,8 +2,7 @@ const app = require('../app');
 const { DEV_PORT } = require('../common/config');
 const { connectToDB } = require('../common/db/mongodb');
 
-// const port = process.env.PORT || DEV_PORT;
-const port = 8080;
+const port = process.env.PORT || DEV_PORT;
 
 app.listen(port, () =>
   console.log(`server listening at http://localhost:${port}`)
